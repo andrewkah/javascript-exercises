ba5d9988d81d4f7fbb3ea3c98b3e2b32245b6444
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-14 18:18:08 +0300

    Fix repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(repString, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let str = "";
+    let i = 0;
+    while (i < num) {
+        str += repString;
+        i++;
+    }
+    return str;
+    }
 };
 
 // Do not edit below this line
