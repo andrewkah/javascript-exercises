3b2c78b9a28a24cd76752e783b39361f7b2800e8
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-15 12:06:16 +0300

    Add to tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  degCelsius = (degrees - 32) * (5/9)
+  return rounded(degCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  degFahr = ((degrees * (9/5)) + 32)
+  return rounded(degFahr);
 };
+
+function rounded(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
