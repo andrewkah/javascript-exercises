483a2f9e480c8829b8398c99dc37f206c8eb2afb
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-15 11:39:53 +0300

    Add to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    sumVar = 0;
+    if (!(Number.isInteger(first) && Number.isInteger(last) && first >0 && last > 0)) {
+        return 'ERROR';
+    }
+    if (first > last) {
+        a=0;
+        a = first;
+        first = last;
+        last = a;
+    }
+    for (let index = first; index <= last; index++) {
+        sumVar += index;        
+    }
+    return sumVar;
+    
 };
 
 // Do not edit below this line
