353f0d88b0c82a3fbf46c279413dcc0636055d05
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-16 15:51:19 +0300

    Add to palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (reverseString) {
+    palin1 = reverseString.toLowerCase().replace(/[^a-z0-9]/g, "");
+    palin2 = palin1.split('').reverse().join('');
+    if (palin1 == palin2) return true;
+    else return false;
 };
 
 // Do not edit below this line
