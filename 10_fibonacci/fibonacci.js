daf6fb00e69d13b64a847dacef88e079dca2e79c
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-16 15:51:56 +0300

    Add to fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [0,1];
+    if(num<0) return 'OOPS';
+    if (num<=2) return 1;
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+        
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
