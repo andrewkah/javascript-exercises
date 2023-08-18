fb3bc21e214cf2327e2dc6a636a1d67144b3bc69
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-18 11:12:48 +0300

    Add to caesar cipher

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,4 +1,32 @@
-const caesar = function() {
+const caesar = function(text, num) {
+    let arr = [];
+    for (let i = 0; i < text.length; i++) {
+        if (!(/[a-zA-Z]/.test(text[i]))) {
+            {arr[i]=text[i]; continue;}}
+        let key = text.charCodeAt(i) + num;
+        if (text[i] == text[i].toLowerCase()) {
+            if (n>122) {
+                while (n>122) {
+                    {n -=26;}
+                }
+            } else{
+                while (n<97) {
+                    n+=26;
+                }
+            }
+        } else{
+            if (n>90) {
+                n-=26;
+            } else{
+                while (n<65) {
+                    n+=26;
+                }
+            }
+            arr[i]= String.fromCharCode(n);
+        }
+        
+    }
+    return arr.join('');
 
 };
 
