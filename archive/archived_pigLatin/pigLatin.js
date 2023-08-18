57e032fb93c61b5355bbcb276b3374208a51fe7a
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-08-18 11:13:28 +0300

    Add to the pigLatin exercise

--- archive/archived_pigLatin/pigLatin.js
+++ archive/archived_pigLatin/pigLatin.js
@@ -1,5 +1,21 @@
-function pigLatin(string) {
+function pigLatin(str) {
+    return str
+    .split(" ")
+    .map((word) => {
+      const index = firstVowelIndex(word);
+      const beginning = word.slice(0, index);
+      const ending = word.slice(index);
+      return `${ending}${beginning}ay`;
+    })
+    .join(" ");
+};
 
+const firstVowelIndex = function (str) {
+  const vowels = str.match(/[aeiou]/g);
+  if (vowels[0] == "u" && str[str.indexOf(vowels[0]) - 1] == "q") {
+    return str.indexOf(vowels[1]);
+  }
+  return str.indexOf(vowels[0]);
 };
   
 // Do not edit below this line
